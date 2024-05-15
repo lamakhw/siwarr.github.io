bc7f28162d321f40455abeabe37a183ee79322d3
lamakhw <202312109@yu.edu.sa>
2024-05-14 18:23:04 -0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,9 @@
+function changeFont()
+	{
+	document.getElementById("changable").style.fontSize = "25px";
+   
+	}
+	function changeFontBack()
+	{
+	document.getElementById("changable").style.fontSize = "40px";
+	}
